af9a8816f0177b264ee43efc650170fadbcb61c6
shoumik123majumdar <shoumik123majumdar@gmail.com>
2024-12-12 13:58:31 -0500

    Fixed path to image_directory in efs-dps server

--- ViSTA_System/sys_main_gemini.py
+++ ViSTA_System/sys_main_gemini.py
@@ -129,7 +129,7 @@ def generate_metadata(image_front_path,image_processor,transcription_model,image
 def main():
     #Ask for the image_directory they want to process
     image_batch_name = input("Name of image batch directory uploaded to the efs-dps/input directory that you want to be processed:")
-    image_directory = f"efs/home/ec2-user/efs-dps/input/{image_batch_name}"
+    image_directory = f"/home/ec2-user/efs-dps/input/{image_batch_name}"
     manifest = load_manifest(f"{image_directory}/manifest.xlsx")
 
 
