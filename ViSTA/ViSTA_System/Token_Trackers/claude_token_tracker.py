f8a0c2e18fdc627ec23d0ce9d8c6ba6d9319e4ee
shoumik123majumdar <shoumik123majumdar@gmail.com>
2024-12-13 16:29:55 -0500

    implemented update_token_tracker method

--- ViSTA/ViSTA_System/Token_Trackers/claude_token_tracker.py
+++ ViSTA/ViSTA_System/Token_Trackers/claude_token_tracker.py
@@ -7,3 +7,12 @@ class ClaudeTokenTracker(TokenTracker):
         :param token_data: usage_metadata object from Gemini API request
         :return:
         """
+        # Extract token data from the response
+        total_tokens = token_data.get("total_tokens")
+        input_tokens = token_data.get("input_tokens")
+        output_tokens = token_data.get("output_tokens")
+
+        # Update token counts
+        self.update_total_token_count(total_tokens)
+        self.update_total_input_token_count(input_tokens)
+        self.update_total_output_token_count(output_tokens)
